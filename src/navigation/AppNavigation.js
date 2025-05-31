ba4cce6f82caab0ea0b727fef4a2196577b5144a
Andy <aidanagalaxysss@gmail.com>
2025-05-31 17:53:09 +0500

    added DetailScr in AppNavigatrion

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,13 +1,30 @@
 // import { createStaticNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScr } from "../screens/HomeScr";
+import { Text } from "@rneui/themed";
+import { DetailScr } from "../screens/DetailsScr";
 
 const Stack = createNativeStackNavigator();
 
 export const RootStack = () => {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScr} />
+      <Stack.Screen
+        name="Home"
+        component={HomeScr}
+        options={{
+          title: "Главная",
+        }}
+      />
+      <Stack.Screen
+        name="Detail"
+        component={DetailScr}
+        options={{
+          title: "Рецепт",
+          headerBackButtonDisplayMode: "minimal",
+          headerTintColor: "black",
+        }}
+      />
     </Stack.Navigator>
   );
 };
