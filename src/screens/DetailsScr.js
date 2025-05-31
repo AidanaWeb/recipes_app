ba2c1721f7276eb8c93d9db080f5ef58eddc7683
Andy <aidanagalaxysss@gmail.com>
2025-05-31 17:52:48 +0500

    created DetailScr

---
+++ src/screens/DetailsScr.js
@@ -0,0 +1,10 @@
+import { Text } from "@rneui/themed";
+import { View } from "react-native";
+
+export const DetailScr = () => {
+  return (
+    <View>
+      <Text>Detail screen</Text>
+    </View>
+  );
+};
