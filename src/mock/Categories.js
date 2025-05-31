80357a1d080873fb9353cbc2a002c5d80de799d2
Andy <aidanagalaxysss@gmail.com>
2025-05-31 19:06:25 +0500

    added mock files

---
+++ src/mock/Categories.js
@@ -0,0 +1,23 @@
+export const Categories = [
+  {
+    id: "1",
+    name: "Напитки",
+    img: "https://i.guim.co.uk/img/media/73491411c86d5fd7d5a839fcf8d7689410c45218/0_34_6402_5201/master/6402.jpg?width=700&quality=85&auto=format&fit=max&s=91b8a971884b0477b085057138c86ef8",
+    mirror: true,
+  },
+  {
+    id: "2",
+    name: "Мясное",
+    img: "https://thumbs.dreamstime.com/b/grilled-bbq-steak-white-background-close-up-67802337.jpg",
+  },
+  {
+    id: "3",
+    name: "Вегетарианское",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdIytuxhjYsAV8eOlnuQFV7zlwhxAHD1n-Xg&s",
+  },
+  {
+    id: "4",
+    name: "Десерты",
+    img: "https://www.shutterstock.com/image-photo/various-sweets-arranged-on-one-600nw-2313003543.jpg",
+  },
+];
